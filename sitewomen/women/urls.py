ba185a65540d8df87072ac64fe416d8716a25ad9
Volodymyr <terces@ukr.net>
2024-04-23 17:55:14 +0300

    study

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('categories/<slug:cat_slug>', views.categories_by_slug, name='cats'),
     #re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive)  # регулярний вираз
     path('archive/<year4:year>/', views.archive, name='archive'),
+    path('about', views.about, name='about'),
+
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,25 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
-from django.shortcuts import redirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
-def index(request): # HttpRequest
-    return HttpResponse('Page app women.com')
+
+def index(request):  # HttpRequest
+    return render(request, 'women/index.html')
+
+
+def about(request):
+    return render(request, 'women/about.html')
 
 def categories(reqeust, cat_id):
     return HttpResponse(f'<h1> Статті по категоріям </h1><p>id:{cat_id}</p>')
 
+
 def categories_by_slug(request, cat_slug):
     return HttpResponse(f'<h1> Статті по категоріям </h1><p>slug:{cat_slug}</p>')
 
+
 def archive(request, year):
     if year > 2024:
-        #raise Http404()
+        # raise Http404()
         uri = reverse('cats', args=('music',))
         return redirect(uri)
-        #return HttpResponseRedirect(uri)
+        # return HttpResponseRedirect(uri)
 
     return HttpResponse(f'<h1> Архів по рокам </h1><p>slug:{year}</p>')
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1> Сторінка не знайдена</h1>")
